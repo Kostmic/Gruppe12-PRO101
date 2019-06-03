fff1f0963926c65ec66864ee62b98d3b38c9d2b8
Marius <marius.m.j@hotmail.com>
2019-06-04 01:09:52 +0200

    Description
    - Move navbar CSS/JS to 2 external files used by all pages
    - Usage of JS instead of 2 HTML files for login page
    - Ability to switch between floors for the Fjerdingen page
    - Minor consistency changes

--- Innlogging/load.js
+++ Innlogging/load.js
@@ -1,5 +1,9 @@
-window.onload = function() {
+var btn = document.getElementById("button");
+
+btn.addEventListener('click', function() {
+    document.getElementById("loader").style.cssText = "opacity: 1; z-index: 1;";
+    document.getElementById("login-container").style.cssText = "filter: blur(1.5px); -webkit-filter: blur(1.5px);";
     window.setTimeout(function() {
         window.location.href = "../Forside/index.html";
     }, 3000);
-};
+});
